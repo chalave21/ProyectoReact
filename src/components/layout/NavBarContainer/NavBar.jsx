43229f965c90c7af20d09a82d201980e24390340
DESKTOP-OE7RG1K\Usuario <chalave21@outlook.com>
2024-03-07 09:50:28 -0300

    Agregue home, y logo

--- src/components/layout/NavBarContainer/NavBar.jsx
+++ src/components/layout/NavBarContainer/NavBar.jsx
@@ -8,23 +8,35 @@ export default function NavBar() {
   return (
     <AppBar position="static">
       <Toolbar className="navbar-container">
-        <div id="divLogo" className="logo"></div>
+        <div id="divLogo" className="logo">
+          <Link to="/" className="nav-link">
+            <img
+              src="https://res.cloudinary.com/dxe1ly9oz/image/upload/v1709815453/images_czyxeb.png"
+              alt="logo"
+            />
+          </Link>
+        </div>
         <div id="divItems" className="nav-items">
           <ul>
+            <li>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
             <li>
               <Link to="category/mujeres" className="nav-link">
                 Mujeres
               </Link>
             </li>
             <li>
-              <a href="/category/hombres" className="nav-link">
+              <Link to="/category/hombres" className="nav-link">
                 Hombres
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/category/kids" className="nav-link">
+              <Link to="/category/kids" className="nav-link">
                 Kids
-              </a>
+              </Link>
             </li>
           </ul>
         </div>

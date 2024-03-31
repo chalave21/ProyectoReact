d81c2c4f0d946a58d5157744f994283c8b1093db
DESKTOP-OE7RG1K\Usuario <chalave21@outlook.com>
2024-03-31 19:58:14 -0300

    cambio de icono y estilo del cartWidget

--- src/components/common/CartWidgetContainer/Cartwidget.jsx
+++ src/components/common/CartWidgetContainer/Cartwidget.jsx
@@ -1,14 +1,39 @@
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+  "& .MuiBadge-badge.MuiBadge-colorSecondary": {
+    backgroundColor: "orange",
+  },
+  " & .MuiSvgIcon-root": {
+    color: "white",
+  },
+}));
 function Cartwidget() {
   const { getTotalItem } = useContext(CartContext);
   let total = getTotalItem();
   return (
     <div>
-      <AddShoppingCartIcon />
-      <p>{total}</p>
+      <IconButton
+        aria-label="cart"
+        // sx={{
+        //   color: "white",
+        // }}
+      >
+        <StyledBadge badgeContent={total} color="secondary">
+          <ShoppingCartIcon />
+        </StyledBadge>
+      </IconButton>
     </div>
   );
 }
